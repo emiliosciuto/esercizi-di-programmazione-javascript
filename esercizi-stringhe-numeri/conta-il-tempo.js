744957cd3eccff5b9edb0ac03ae1debdb2016b61
emiliosciuto <49308835+emiliosciuto@users.noreply.github.com>
2019-04-05 18:18:37 +0200

    Update conta-il-tempo.js

--- esercizi-stringhe-numeri/conta-il-tempo.js
+++ esercizi-stringhe-numeri/conta-il-tempo.js
@@ -12,3 +12,18 @@
 
   http://www.imparareaprogrammare.it
 */
+
+
+var tempo = 12560;
+
+var ore = (tempo/3600);
+var a = Math.round(ore);
+
+var minuti = (ore-a) * 60;
+var b = Math.round(minuti);
+
+var secondi = (minuti-b) * 60;
+var c = Math.round(secondi);
+
+
+console.log(a + ' ore', b + ' minuti e', c +' secondi');
